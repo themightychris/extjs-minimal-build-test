4f000af6335a8e115d7c2479432faef090c5ea05
Chris Alfano <chris@jarv.us>
2015-04-14 10:33:56 -0400

    added only-xtemplate branch

--- src/MyClass.js
+++ src/MyClass.js
@@ -1,7 +1,11 @@
 Ext.define('MyApp.MyClass', {
 	singleton: true,
+	requires: [
+		'Ext.XTemplate'
+	],
 
     sayHello: function() {
         alert('hello');
+        // Ext.GlobalEvents;
     }
 });
